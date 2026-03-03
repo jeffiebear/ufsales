18ff04ae9cb89122cbdb7227fb682348b2b56325
osirisnunez <osiris@goparameter.com>
2026-03-03 10:13:42 -0500

    ufs branding overrides

---
+++ uf_sales_website/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "UF Sales Website Styles",
+    "version": "19.0.1.0.0",
+    "category": "Website",
+    "summary": "UF Sales brand fonts/colors and frontend styling for website + ecommerce",
+    "license": "LGPL-3",
+    "author": "Parameter",
+    "website": "https://parameterllc.com",
+    "depends": [
+        "website",
+        "website_sale",
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+    "assets": {},
+    "installable": True,
+    "application": False,
+}
