48324c3ae398b0b1792fa48280d4a66f5cd6b3d2
osirisnunez <osiris@goparameter.com>
2026-06-18 14:00:36 -0400

    Update __manifest__.py

--- ufs_alerts/__manifest__.py
+++ ufs_alerts/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'UFS Alerts',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'summary': 'Margin-floor and low-stock alerting for UFS workflows.',
     'description': """
 UFS Alerts
